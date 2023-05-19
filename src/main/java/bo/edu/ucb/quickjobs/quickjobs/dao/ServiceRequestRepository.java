b25998753c3615bb2215d5340d263332d0ceaec3
Jose Pozo Silva <jhosede23@gmail.com>
2023-05-18 20:19:20 -0400

    adding last files of DAO

---
+++ src/main/java/bo/edu/ucb/quickjobs/quickjobs/dao/ServiceRequestRepository.java
@@ -0,0 +1,5 @@
+package bo.edu.ucb.quickjobs.quickjobs.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ServiceRequestRepository extends JpaRepository<ServiceRequestRepository, Long>{
+}
---
+++ src/main/java/bo/edu/ucb/quickjobs/quickjobs/dao/UserRepository.java
@@ -0,0 +1,5 @@
+package bo.edu.ucb.quickjobs.quickjobs.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<UserRepository, Long>{
+}
